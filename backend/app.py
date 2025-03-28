5125c06688855c3c82377783f4542662ead8a603
gitaehee <rainwifekth@naver.com>
2025-03-28 15:07:45 +0900

    fix: render 호환되게 포트 코드 수정

--- backend/app.py
+++ backend/app.py
@@ -68,4 +68,5 @@ def predict_api():
     return jsonify({"top3": results})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # ✅ Render 호환용
+    app.run(host="0.0.0.0", port=port)
